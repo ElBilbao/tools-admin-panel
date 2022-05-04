01c1eb5f743b9093a0d761cd692ab4fa2dfe809e
Rodrigo Bilbao <rbilbao98@gmail.com>
2022-05-04 15:00:54 -0500

    Fix existing form, added serial number field

--- web/app/pages/forms/submit-old-tool.js
+++ web/app/pages/forms/submit-old-tool.js
@@ -44,6 +44,20 @@ function ExistingForm() {
   return (
     <form class="w-full max-w-lg">
       <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3 mb-6 md:mb-0">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-first-name"
+          >
+            Serial Number (ID)
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name"
+            type="text"
+            placeholder="UX2932"
+          />
+        </div>
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
