73fcee35d4eb749f3c085f8eb39aaa026b68b6c7
betorendon <betorendonruiz@gmail.com>
2022-05-05 15:25:26 -0500

    Added status to purchase tool form

--- web/app/pages/forms/submit-purchase-tool.js
+++ web/app/pages/forms/submit-purchase-tool.js
@@ -194,13 +194,13 @@ function PurchaseToolForm() {
         </div>
 
 
-        {/* Notes */}
-        <div class="w-full px-3 mb-6 md:mb-0">
+        {/* Purchase Item Notes */}
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             for="grid-first-name"
           >
-            Purchased Item Notes
+            Purchase Item Notes
           </label>
           <input
             class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
@@ -209,6 +209,39 @@ function PurchaseToolForm() {
             placeholder="XXXXXXXX"
           />
         </div>
+
+
+
+
+        {/* Status */}
+        <div class="w-full md:w-1/2 px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-state"
+          >
+            Status
+          </label>
+          <div class="relative">
+            <select
+              class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-state"
+            >
+              <option>Available</option>
+              <option>Approved</option>
+              <option>Not available</option>
+            </select>
+            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg
+                class="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+
 
 
 
